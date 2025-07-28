add scaling, fix some bugs

src/data/dataset.py
@@ -28,8 +28,10 @@ class BioTrainingDataset(torch.utils.data.Dataset):
         self._df = self._df[self._df['token_num'] > 20]
         self._df.sort_values('token_num', ascending=False)
         self._data = self._df['processed_path'].tolist()
+        self._embedding = self._df['embedding_path'].tolist()
 
         self.data = np.asarray(self._data)
+        self.embedding = np.asarray(self._embedding)
         self.transform = transform
         self.training = training  # not implemented yet
 
@@ -53,6 +55,8 @@ class BioTrainingDataset(torch.utils.data.Dataset):
 
         with gzip.open(data_path, 'rb') as f:
             data_object = pickle.load(f)
+
+        data_object['plm_embedding'] = torch.load(self.embedding[idx])
 
         if self.transform is not None:
             data_object = self.transform(data_object)
@@ -106,10 +110,10 @@ class BioInferenceDataset(torch.utils.data.Dataset):
         if data_path.endswith('.pkl.gz'):
             with gzip.open(data_path, 'rb') as f:
                 data_object = pickle.load(f)
-        # elif data_path.endswith('.pdb') or data_path.endswith('.cif'):
-        #     data_object = get_cg_repr(get_structure(data_path))
         else:
             raise ValueError(f"Unsupported file format: {data_path}")
+
+        data_object['plm_embedding'] = torch.load(self.embedding[idx])
 
         if self.transform is not None:
             data_object = self.transform(data_object)

src/model/residue_generate_network.py
@@ -22,6 +22,7 @@ class ResGenNet(nn.Module):
         z_template: int = 128,
         n_layers: int = 8,
         n_attn_heads: int = 8,
+        sigma_data: float = 16.0
     ):
         super(ResGenNet, self).__init__()
 
@@ -63,14 +64,20 @@ class ResGenNet(nn.Module):
 
         nn.init.zeros_(self.linear_no_bias_o.weight)
 
+        self.sigma_data = sigma_data
+
     def forward(
         self,
         noisy_structure: torch.Tensor,
         plm_embedding: torch.Tensor,
         feature_dict: Dict[str, torch.Tensor],
-        noise_scale: float,
+        noise_scale: torch.Tensor,
         template: Optional[torch.Tensor],
     ):
+        # scaling noisy structure
+        noisy_structure = noisy_structure / torch.sqrt(self.sigma_data ** 2 + noise_scale ** 2)[..., None, None]
+
+        # network part
         s_embed, z_embed = self.seq_embedder(plm_embedding, feature_dict, noise_scale)
 
         a_embed = self.linear_no_bias_a(noisy_structure)
@@ -85,6 +92,13 @@ class ResGenNet(nn.Module):
             z=z_embed,
         )
         a_out = self.linear_no_bias_o(self.layer_norm_o(a_out))
+
+        # reverse-scaling for output structure
+        s_ratio = (noise_scale / self.sigma_data)[..., None, None].to(a_out.dtype)
+        a_out = (
+            1 / (1 + s_ratio ** 2) * noisy_structure
+            + noise_scale[..., None, None] / torch.sqrt(1 + s_ratio ** 2) * a_out
+        ).to(a_out.dtype)
 
         return a_out
 

src/train.py
@@ -98,6 +98,7 @@ def main(args: DictConfig):
         z_template=args.model.z_template,
         n_layers=args.model.n_layers,
         n_attn_heads=args.model.n_attn_heads,
+        sigma_data=args.sigma_data,
     ).to(device)
 
     # initialize diffusion sampler
@@ -286,8 +287,6 @@ def main(args: DictConfig):
             for crt_val_step, val_feature_dict in val_iter:
                 torch.cuda.empty_cache()
                 val_feature_dict = to_device(val_feature_dict, device)
-
-                N_sample = args.num_sample
                 s_inputs = val_feature_dict['plm_embedding']
 
                 label_dict = {
@@ -305,7 +304,7 @@ def main(args: DictConfig):
                     label_dict=label_dict,
                     input_feature_dict=val_feature_dict,
                     s_inputs=s_inputs,
-                    N_sample=N_sample,
+                    N_sample=training_sample,
                     diffusion_chunk_size=None,
                 )
 